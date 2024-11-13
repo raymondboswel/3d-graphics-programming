ce9f7a2f440cafb6bf950c3f075c0783551445c6
Raymond Boswel <raymondboswel@gmail.com>
2024-11-13 13:46:08 +1300

    Add projection of points

--- src/main.c
+++ src/main.c
@@ -12,7 +12,9 @@ bool is_running = false;
 #define N_POINTS 9 * 9 * 9
 vec3_t cube_points[N_POINTS];
 vec2_t projected_points[N_POINTS];
-float fov_factor = 128;
+float fov_factor = 640;
+
+vec3_t camera_position = {.x = 0, .y = 0, .z = -5};
 
 void setup(void) {
   // Allocate the required memory bytes to hold the color buffer
@@ -51,15 +53,32 @@ void process_input(void) {
   }
 }
 
-vec2_t project(vec3_t point) {
-  vec2_t projected_point = {fov_factor * point.x, fov_factor * point.y};
+//
+// Function that receives a 3D vector and returns an orthographically projected
+// 2D point
+//
+vec2_t project_orthographic(vec3_t point) {
+  vec2_t projected_point = {.x = fov_factor * point.x,
+                            .y = fov_factor * point.y};
+  return projected_point;
+}
+
+//
+// Function that receives a 3D vector and returns an orthographically projected
+// 2D point
+//
+vec2_t project_perspective(vec3_t point) {
+  vec2_t projected_point = {.x = fov_factor * point.x / point.z,
+                            .y = fov_factor * point.y / point.z};
   return projected_point;
 }
 
 void update(void) {
   for (int i = 0; i < N_POINTS; i++) {
     vec3_t point = cube_points[i];
-    vec2_t projected_point = project(point);
+    // Move the points away from the camera
+    point.z -= camera_position.z;
+    vec2_t projected_point = project_perspective(point);
     projected_points[i] = projected_point;
   }
 }
@@ -70,8 +89,8 @@ void render(void) {
   // Loop all projected_points and render them
   for (int i = 0; i < N_POINTS; i++) {
     vec2_t projected_point = projected_points[i];
-    draw_rect(projected_point.x + (window_width / 2),
-              projected_point.y + (window_height / 2), 4, 4, 0xFFFFFF00);
+    draw_rect(projected_point.x + ((float)window_width / 2),
+              projected_point.y + ((float)window_height / 2), 4, 4, 0xFFFFFF00);
   }
 
   render_color_buffer();
